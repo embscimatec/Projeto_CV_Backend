cbe342d365b02a7d379c78be60edb68e4297f0da
fernandanlisboa <fernandanlisboa@gmail.com>
2020-10-12 09:42:06 -0300

    criando model ContatoEmergencia

---
+++ app/models/ContatoEmergencia.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, Text, DateTime, Float, Boolean, PickleType
+from app import db
+
+class ContatoEmergencia(db.Model):
+    __tablename__ = "contatoEmergencia"
+    
+    contatoEmergenciaId = db.Column(db.Integer, primary_key = True)
+    nome = db.Column(db.String(50), nullable = False)
+    relacao = db.Column(db.String(20), nullable = False)
+    email = db.Column(db.String(80), nullable = False)
+    
+    def __init__(self, nome, relacao, email):
+        self.nome = nome
+        self.relacao = relacao
+        self.email = email
+    
