7416575cee464914c091bfca3a7dbef659f3e81a
fernandanlisboa <fernandanlisboa@gmail.com>
2020-10-12 09:46:33 -0300

    criando model de Imagem

---
+++ app/models/Imagem.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, Text, DateTime, Float, Boolean, PickleType
+from app import db
+
+class Imagem(db.Model):
+    __tablename__ = "imagem"
+    
+    imagemId = db.Column(db.Integer, primary_key = True)
+    encode = db.Column(db.String, nullable = False)
+    url = db.Column(db.String, nullable = False)
+    extensao = db.Column(db.String(10), nullable = False)
+    
+    def __init__(self, encode, url, extensao):
+        self.encode = encode
+        self.url = url
+        self.extensao = extensao
