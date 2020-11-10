2b68ece4e62ae541cf55bb13caa6fa908b499130
AmadoMaria <mariantoniamado@hotmail.com>
2020-11-10 15:03:11 -0300

    preparando ambiente

--- app.py
+++ app.py
@@ -1,4 +1,34 @@
-from app import manager
-#método de segurança
-if __name__ == "__main__":
-    manager.run()
+from flask import Flask, jsonify
+from flask_restful import Api
+from resources.hotel import Hoteis, Hotel
+from resources.usuario import User, UserRegister, UserLogin, UserLogout
+from flask_jwt_extended import JWTManager
+from blacklist import BLACKLIST
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/dbVisao'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'DontTellAnyone'
+app.config['JWT_BLACK_LIST_ENABLED'] = True # Ativação da BLACKLIST, permitindo invalidar algum ID
+api = Api(app)
+jwt = JWTManager(app)
+
+
+@app.before_first_request
+def cria_banco():
+    banco.create_all()
+
+@jwt.token_in_blacklist_loader
+def verifica_blacklist(token):
+    return token['jti'] in BLACKLIST
+
+@jwt.revoked_token_loader
+def token_acesso_invalidado():
+    return jsonify({'message': 'You have been logged out'}), 401
+
+
+if __name__ == '__main__':
+    from sql_alchemy import banco
+
+    banco.init_app(app)
+    app.run(debug=True)
